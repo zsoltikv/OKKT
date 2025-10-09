bfe0f8c49861e4a22b8b9d9819c5d916823c3b98
zsoltikaa <padarzsolti@gmail.com>
2025-10-09 11:12:58 +0200

    screen oritentation landscape fix.

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -4,8 +4,19 @@ using Android.OS;
 
 namespace OKKT25
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
+    [Activity(
+        Theme = "@style/Maui.SplashTheme",
+        MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges = ConfigChanges.ScreenSize
+                             | ConfigChanges.Orientation
+                             | ConfigChanges.UiMode
+                             | ConfigChanges.ScreenLayout
+                             | ConfigChanges.SmallestScreenSize
+                             | ConfigChanges.Density,
+        ScreenOrientation = ScreenOrientation.Landscape
+    )]
+    public class MainActivity : Microsoft.Maui.MauiAppCompatActivity
     {
     }
 }
