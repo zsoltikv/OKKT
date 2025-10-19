981187c99c4fca12cb7ca23e276c4492c8fe904f
zsoltikaa <padarzsolti@gmail.com>
2025-10-19 20:36:21 +0200

    fixed a minor issue regarding font colors when switching Android device theme.

--- Views/App.xaml.cs
+++ Views/App.xaml.cs
@@ -1,15 +1,17 @@
 ﻿namespace OKKT25
 {
+
     public partial class App : Application
     {
-
         [Obsolete]
         public App()
         {
             InitializeComponent();
+
+            Application.Current.UserAppTheme = AppTheme.Dark;
+
             MainPage = new AppShell();
         }
-
     }
 
 }
